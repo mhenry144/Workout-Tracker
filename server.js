fc78de28d4a81e8635d6162dccbbeb6c4c1ea3cb
Matthew Henry <tadaemusic@gmail.com>
2020-07-08 12:45:17 -0400

    added html routes

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+
+
 app.listen(5000);
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
