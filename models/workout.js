05b33ac23e9d3d2f05ebe96e3bab4e774d265532
Matthew Henry <tadaemusic@gmail.com>
2020-07-10 15:22:17 -0400

    created workout model

--- models/workout.js
+++ models/workout.js
@@ -1,8 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-var workoutSchema = new mongoose.Schema({
+// create database schema
+const workoutSchema = new mongoose.Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name",
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes",
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    // convert objects into strings with JSON format, including virtual properties
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
-})
+// add total workout duration variable
+workoutSchema.virtual("totalDuration").get(function () {
+  // reduce excercise array to display only total durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
-const path = require('path');
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
-
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.json());
 
-
-
+var mongoConfig = process.env.MONGODB_URI || "mongodb://localhost/workoutDB";
+mongoose.connect(
+  mongoConfig,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded.");
+    } else {
+      console.log("Error in DB connection : " + err);
+    }
+  }
+);
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
