8f23e40a7f3f3f3b860bf54a38ce5ccb41011cd0
Matthew Henry <tadaemusic@gmail.com>
2020-07-10 13:07:40 -0400

    added routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,58 @@
+const Workout = require("../models/workout");
+
+module.exports = function (app) {
+  
+    //GET REQUESTS
+
+  app.get("/api/workouts", function (req, res) {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.get("/api/workouts/range", function (req, res) {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  //POST REQUESTS
+
+  app.post("/api/workouts", function (req, res) {
+    Workout.create({})
+      .then((data) => res.json(data))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.post("/api/workouts/range", function (req, res) {
+    Workout.create({})
+      .then((data) => res.json(data))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  //PUT REQUESTS
+
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then((data) => res.json(data))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const app = express();
 
 
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
 
 
-app.listen(5000);
+
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+
+app.listen(5000);
